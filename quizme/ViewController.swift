562c8d99fd4384036fd87f90c09140759b00187b
Macbook <macbook@macbooks-macbook-pro-8.local>
2018-05-10 18:47:29 -0500

    Initial Commit

---
+++ quizme/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  quizme
+//
+//  Created by Macbook on 5/10/18.
+//  Copyright © 2018 Yohana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
